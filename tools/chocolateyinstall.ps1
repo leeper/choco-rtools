2c79ee577e8008d0425b9208372e0ced51f37490
Thomas J. Leeper <thosjleeper@gmail.com>
2016-02-03 14:28:45 +0000

    add v2.7.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿$packageName= 'rtools'
-$url        = 'https://stat.ethz.ch/CRAN/bin/windows/Rtools/Rtools26.exe'
+$url        = 'https://stat.ethz.ch/CRAN/bin/windows/Rtools/Rtools27.exe'
 
 Install-ChocolateyPackage $name 'exe' $url
